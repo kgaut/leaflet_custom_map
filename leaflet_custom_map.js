4da7e2bde4b747aeed36dfd67725a27c0b6fba9e
Kevin Gautreau <contact@kgaut.net>
2019-04-16 17:45:33 +0200

    Update js integration

---
+++ leaflet_custom_map.js
@@ -0,0 +1,14 @@
+(function ($, Drupal, L) {
+
+    jQuery(document).bind('leaflet.map', function(event, map, lMap) {
+        if(Drupal.settings.leaflet_custom_map.url !== null) {
+            var imageUrl = Drupal.settings.leaflet_custom_map.url;
+            var imageBounds = JSON.parse(Drupal.settings.leaflet_custom_map.bounds);
+
+            var layer = L.imageOverlay(imageUrl, imageBounds);
+            layer.addTo(lMap);
+        }
+    });
+
+})(jQuery, Drupal, window.L);
+
---
+++ lib/CustomMap.php
@@ -0,0 +1,20 @@
+<?php
+
+class CustomMap extends MyObject {
+
+  protected static $db_table_name = 'custom_map';
+
+  protected static $db_table_identifier = 'cmid';
+
+  public $cmid;
+
+  public $fid;
+
+  public $map;
+
+  public $bounds;
+
+  public $created;
+
+  public $user;
+}
