2e33b2e8ab6f09c81c278a653a7a03d1775fc224
Kevin Gautreau <contact@kgaut.net>
2019-04-26 19:50:44 +0200

    Gestion poids

--- lib/CustomMap.php
+++ lib/CustomMap.php
@@ -20,11 +20,47 @@ class CustomMap extends MyObject {
 
   public $opacity;
 
+  public $weight;
+
   public $created;
 
   public $user;
 
   public static function loadForMap($map) {
-    return self::_load([['map', $map], ['status', 1]], TRUE);
+    $objectsArray = array();
+    $query = db_select(static::$db_table_name, 't');
+    $query->fields('t');
+    $query->condition('map', $map);
+    $query->condition('status', 1);
+    $query->orderBy('weight', 'ASC');
+
+    $result = $query->execute();
+    if ($result) {
+      while ($row = $result->fetchObject()) {
+        $instance = new static();
+        foreach ($instance as $k => &$v) {
+          if (isset($row->{$k})) {
+            $v = $row->{$k};
+          }
+        }
+        $objectsArray[] = $instance;
+      }
+    }
+
+    return $objectsArray;
   }
+
+  public static function loadAll() {
+    $steps = array();
+    $query = db_select(static::$db_table_name, 's');
+    $query->fields('s', array(static::$db_table_identifier));
+    $query->orderBy('map', 'ASC');
+    $query->orderBy('weight', 'ASC');
+    $result = $query->execute();
+    while ($row = $result->fetchObject()) {
+      $steps[] = new static(array(static::$db_table_identifier => $row->{static::$db_table_identifier}));
+    }
+    return $steps;
+  }
+
 }
